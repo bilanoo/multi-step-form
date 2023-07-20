74d37077e8032f291692bc062d12eeb3b19c78b6
bilanoo <blalkhan94@gmail.com>
2023-07-20 08:37:40 +0100

    added background image for the step container.

--- src/StepSelection/index.tsx
+++ src/StepSelection/index.tsx
@@ -1,11 +1,6 @@
 import "./StepSelection.css";
-import SideBarMobile from "../assets/images/bg-sidebar-mobile.svg";
 const StepSelection = () => {
-  return (
-    <div className="step-selection-container">
-      <img src={SideBarMobile} alt="sidebar" className="sidebar" />
-    </div>
-  );
+  return <div className="step-selection-container"></div>;
 };
 
 export default StepSelection;
