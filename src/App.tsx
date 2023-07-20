2e6df350a2bb22e38858c6c95898b77eceab5049
bilanoo <blalkhan94@gmail.com>
2023-07-20 08:29:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import StepSelection from "./StepSelection";
+
+function App() {
+  return (
+    <>
+      <main>
+        <StepSelection />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/StepSelection/index.tsx
@@ -0,0 +1,11 @@
+import "./StepSelection.css";
+import SideBarMobile from "../assets/images/bg-sidebar-mobile.svg";
+const StepSelection = () => {
+  return (
+    <div className="step-selection-container">
+      <img src={SideBarMobile} alt="sidebar" className="sidebar" />
+    </div>
+  );
+};
+
+export default StepSelection;
