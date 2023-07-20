62f68741531c0bdbd74a6d78256aa6e21aab7be3
bilanoo <blalkhan94@gmail.com>
2023-07-20 09:58:03 +0100

    implemented desktop versions with text information of each step and desktop container.

--- src/StepSelection/index.tsx
+++ src/StepSelection/index.tsx
@@ -3,10 +3,34 @@ const StepSelection = () => {
   return (
     <div className="selection-container">
       <div className="steps">
-        <button className="selection-button">1</button>
-        <button className="selection-button">2</button>
-        <button className="selection-button">3</button>
-        <button className="selection-button">4</button>
+        <div className="button-container">
+          <button className="selection-button">1</button>
+          <div className="steps-information">
+            <p className="step">STEP 1</p>
+            <p className="step-detail">YOUR INFO</p>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="selection-button">2</button>
+          <div className="steps-information">
+            <p className="step">STEP 2</p>
+            <p className="step-detail">SELECT PLAN</p>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="selection-button">3</button>
+          <div className="steps-information">
+            <p className="step">STEP 3</p>
+            <p className="step-detail">ADD-ONS</p>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="selection-button">4</button>
+          <div className="steps-information">
+            <p className="step">STEP 4</p>
+            <p className="step-detail">SUMMARY</p>
+          </div>
+        </div>
       </div>
     </div>
   );
