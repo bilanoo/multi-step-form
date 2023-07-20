a7ee69f59dab18a0361c736328c9506a6c31b5d5
bilanoo <blalkhan94@gmail.com>
2023-07-20 08:58:05 +0100

    added steps buttons and styled
    mobile version

--- src/StepSelection/index.tsx
+++ src/StepSelection/index.tsx
@@ -1,6 +1,15 @@
 import "./StepSelection.css";
 const StepSelection = () => {
-  return <div className="step-selection-container"></div>;
+  return (
+    <div className="selection-container">
+      <div className="steps">
+        <button className="selection-button">1</button>
+        <button className="selection-button">2</button>
+        <button className="selection-button">3</button>
+        <button className="selection-button">4</button>
+      </div>
+    </div>
+  );
 };
 
 export default StepSelection;
