9954534e72c4d3764a835dffaa38add87c50c120
bilanoo <blalkhan94@gmail.com>
2023-07-21 18:12:49 +0100

    content for second page completed, made reusable plan type component where i am just passing down values.

---
+++ src/PlanTypeContent/PlanType/index.tsx
@@ -0,0 +1,57 @@
+import "./planType.css";
+
+interface PlanTypeProps {
+  iconSource: string;
+  planName: string;
+  planPrice: string;
+}
+export const PlanType = ({
+  iconSource,
+  planName,
+  planPrice,
+}: PlanTypeProps) => {
+  return (
+    <>
+      <div className="plan-type-container">
+        <input
+          type="radio"
+          name={"plan-type"}
+          id={planName}
+          value={planName}
+        ></input>
+
+        <label htmlFor={planName}>
+          <img
+            src={iconSource}
+            alt="plan-type-icon"
+            className="plan-type-icon"
+          />
+          <div className="plan-information">
+            <p
+              style={{
+                color: "hsl(213, 96%, 18%)",
+                fontWeight: "700",
+                padding: "0",
+                margin: "0",
+                alignSelf: "center",
+                paddingLeft: "10px",
+              }}
+            >
+              {planName}
+            </p>
+            <p
+              style={{
+                color: "hsl(231, 11%, 63%)",
+                margin: "0",
+                paddingLeft: "10px",
+                fontSize: ".875rem",
+              }}
+            >
+              ${planPrice}/mo
+            </p>
+          </div>
+        </label>
+      </div>
+    </>
+  );
+};
--- src/PlanTypeContent/index.tsx
+++ src/PlanTypeContent/index.tsx
@@ -1,3 +1,22 @@
+import { PlanType } from "./PlanType";
+import "./PlanTypeContent.css";
+
+import GameIcon from "../../src/assets/images/icon-arcade.svg";
+import IconAdvanced from "../../src/assets/images/icon-advanced.svg";
+import IconPro from "../../src/assets/images/icon-pro.svg";
+
 export const PlanTypeContent = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="all-plans-containers">
+        <PlanType iconSource={GameIcon} planName="Arcade" planPrice="9" />
+        <PlanType
+          iconSource={IconAdvanced}
+          planName="Advanced"
+          planPrice="12"
+        />
+        <PlanType iconSource={IconPro} planName="Pro" planPrice="15" />
+      </div>
+    </>
+  );
 };
