2ef238fa5b0cef87a7412465253e59fb03500c21
bilanoo <blalkhan94@gmail.com>
2023-07-21 18:39:14 +0100

    implemented desktop version of the plan types.

--- src/PlanTypeContent/PlanType/index.tsx
+++ src/PlanTypeContent/PlanType/index.tsx
@@ -33,7 +33,6 @@ export const PlanType = ({
                 fontWeight: "700",
                 padding: "0",
                 margin: "0",
-                alignSelf: "center",
                 paddingLeft: "10px",
               }}
             >
